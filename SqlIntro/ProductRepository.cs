09bdbdff9234a4fe65f21b15b6a49f81bb1f8206
Starla Oden <starlaoh@gmail.com>
2018-02-14 11:21:42 -0800

    Opened connection and added Select statement

--- SqlIntro/ProductRepository.cs
+++ SqlIntro/ProductRepository.cs
@@ -26,8 +26,11 @@ namespace SqlIntro
         {
             using (var conn = new MySqlConnection(_connectionString))
             {
+                conn.Open();
+
+
                 var cmd = conn.CreateCommand();
-                cmd.CommandText = "SELECT * products"; //TODO:  Write a SELECT statement that gets all products
+                cmd.CommandText = "SELECT * FROM products"; //TODO:  Write a SELECT statement that gets all products
                 var dr = cmd.ExecuteReader();
                 while (dr.Read())
                 {
