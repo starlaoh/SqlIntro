1d4d13a55a1ab820e97cbd59ec84ed8093cac495
Starla Oden <starlaoh@gmail.com>
2018-02-15 11:58:55 -0800

    Implemented Dapper

---
+++ SqlIntro/DapperProjectRepository.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using MySql.Data.MySqlClient;
+
+namespace SqlIntro
+{
+    class DapperProjectRepository : IProductRepository
+    {
+        private readonly string _connectionString;
+
+    public DapperProjectRepository(string connectionString)
+    {
+        _connectionString = connectionString;
+    }
+
+    /// <summary>
+    /// Reads all the products from the products table
+    /// </summary>
+    /// <returns></returns>
+    public IEnumerable<Product> GetProducts()
+    {
+        using (var conn = new MySqlConnection(_connectionString))
+        {
+            conn.Open();
+            return conn.Query<Product>("SELECT Name FROM product");
+        }
+    }
+
+    /// <summary>
+    /// Deletes a Product from the database
+    /// </summary>
+    /// <param name="id"></param>
+    public void DeleteProduct(int id)
+    {
+        using (var conn = new MySqlConnection(_connectionString))
+        {
+            conn.Open();
+            conn.Execute("DELETE FROM product WHERE ProductId = @Id", new {id});
+        }
+    }
+
+        /// <summary>
+        /// Updates the Product in the database
+        /// </summary>
+        /// <param name="prod"></param>
+        public void UpdateProduct(Product prod)
+    {
+        //This is annoying and unnecessarily tedious for large objects.
+        //More on this in the future...  Nothing to do here..
+        using (var conn = new MySqlConnection(_connectionString))
+        {
+            conn.Open();
+            conn.Execute("UPDATE product SET name = @name WHERE ProductId = @Id", new { prod.Name});
+        }
+    }
+
+        /// <summary>
+        /// Inserts a new Product into the database
+        /// </summary>
+        /// <param name="prod"></param>
+        public void InsertProduct(Product prod)
+    {
+        using (var conn = new MySqlConnection(_connectionString))
+        {
+            conn.Open();
+            conn.Query<Product>("INSERT INTO product (name) values(@name)");
+        }
+
+        Console.ReadKey();
+        }
+}
+}
